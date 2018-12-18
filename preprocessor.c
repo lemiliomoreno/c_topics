ad81f9f7c7cd26b267ff8f3cc196f9a7ed84d850
lemiliomoreno <lemiliomoreno@gmail.com>
2018-12-18 12:56:32 -0600

    Update preprocessor.c

--- preprocessor.c
+++ preprocessor.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Defining a single value */
 #define VALUE (5 * 10)
 
+/* Defining a function, if x > y, print x, if not, print y */
 #define GREATER(x, y) ((x) > (y) ? (x) : (y))
 
+/* Defining a value, with ifndef, define, endif, this is used in the HEADERS
+ * like: 
+ * #ifndef _HEADER_H_
+ * #define _HEADER_H_
+ * ...function declarations...
+ * #endif
+ */
 #ifndef NULL
 #define NULL (void *)0
 #endif //ifndef NULL
@@ -20,11 +29,12 @@ typedef struct {
         int height;
 }person;
 
+/* Defining access to a structure with pasting tokens */
 #define FIELD(field) Emilio.name.field
 
 int main (void)
 {
-
+        /* If 0 won't compile, if 1 will compile */
         #if 0
 
         printf("First part of the code is being executed: %d\n", VALUE + 1);
@@ -44,8 +54,11 @@ int main (void)
         strcpy(Emilio.name.first_name, "Luis");
         strcpy(Emilio.name.last_name, "Moreno");
         printf("Name of the person: %s %s\n", Emilio.name.first_name, Emilio.name.last_name);
+        
+        /* Printing values with tokens */
         printf("Name of the person: %s %s\n", FIELD(first_name), FIELD(last_name));
-
+        
+        /* They MUST end with #endif */
         #endif
 
         return 0;
