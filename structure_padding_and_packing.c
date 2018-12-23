3a4d480c329ab3869b5b36162d5ab8f0e0af9414
lemiliomoreno <lemiliomoreno@gmail.com>
2018-12-23 17:07:09 -0600

    Update structure_padding_and_packing.c

--- structure_padding_and_packing.c
+++ structure_padding_and_packing.c
@@ -57,7 +57,6 @@ int main (void)
 	printf("Size of struct2: %d bytes\n", (int)sizeof(struct3));
 	printf("Size of struct2 packed: %d bytes, saved: %.2f%%\n", (int)sizeof(struct4), 100 - ((float)sizeof(struct4) / (float)sizeof(struct3) * 100));
 
-
 	/* Imagine we need to allocate the space of 100 structs, the packed structure
 	 * will be better because it will allocate less memory, let's try to check for
 	 * the memory stored with 100 structures of ps1 and ps2 with Valgrind:
