0030b7d5cf7b37d9ec21bfa2ffd96e09dd93f285
lemiliomoreno <lemiliomoreno@gmail.com>
2019-01-02 10:50:06 -0600

    Update structure_padding_and_packing.c

--- structure_padding_and_packing.c
+++ structure_padding_and_packing.c
@@ -2,6 +2,10 @@
  * Available at: http://www.catb.org/esr/structure-packing/ 
  */
 
+/* To compile:
+ * gcc -Wall -std=c11 structure_padding_and_packing.c -o structure_padding_and_packing
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -62,7 +66,7 @@ int main (void)
 	/* Imagine we need to allocate the space of 100 structs, the packed structure
 	 * will be better because it will allocate less memory, let's try to check for
 	 * the memory stored with 100 structures of ps1 and ps2 with Valgrind:
-	 * valgrind --leak-check=yes ./structure_padding_and_packing.c
+	 * valgrind --leak-check=yes ./structure_padding_and_packing
 	 * struct 1 will allocate 2400 bytes, meanwhile struct2 will allocate 1600 bytes
 	 */
 	 
